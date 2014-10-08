57d5713347440f266cc89e05c356b012899dcfc2
Øyvind Evju <oyvinev@simula.no>
2014-10-08 14:21:38 +0200

    Clean up and minor rtd fix in docs

--- docs/conf.py
+++ docs/conf.py
@@ -51,17 +51,22 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    # Override default css to get a larger width for local build                
+    def setup(app):                                                              
+        #app.add_javascript("custom.js")                                        
+        app.add_stylesheet('theme_overrides.css')                                
 else:
     sys.path.insert(0, os.getcwd())
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-#if on_rtd: sys.path.insert(0, os.getcwd())
-
+    # Override default css to get a larger width for ReadTheDoc build            
+    html_context = {                                                            
+        'css_files': [                                                          
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',      
+            '_static/theme_overrides.css',                                      
+        ],                                                                      
+    }
 
 html_static_path = ['_static']
-
-def setup(app):
-    # overrides for wide tables in RTD theme
-    app.add_stylesheet('theme_overrides.css') # path relative to _static
 
 # Insert .. to import cbcpost
 sys.path.insert(0, os.path.abspath('..'))

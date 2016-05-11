bb40afb782d69d69250a4db030e628fd9449db2a
Øyvind Evju <oyvinev@simula.no>
2016-05-11 16:29:55 +0200

    Add duplication of meshfunction to workaround problem with different meshes in DomainAvg

--- cbcpost/metafields/DomainAvg.py
+++ cbcpost/metafields/DomainAvg.py
@@ -18,7 +18,10 @@
 
 from cbcpost.fieldbases.MetaField import MetaField
 from cbcpost.utils.utils import cbc_warning
-from dolfin import assemble, dx, Function, Constant, Measure, MeshFunctionSizet
+from cbcpost.utils.mpi_utils import gather, broadcast
+from dolfin import (assemble, dx, Function, Constant, Measure, MeshFunctionSizet, MeshFunctionDouble,
+                    MeshFunctionBool, MeshFunctionInt, MeshFunction)
+import numpy as np
 
 def _init_measure(measure="default", cell_domains=None, facet_domains=None, indicator=None):
     assert cell_domains == None or facet_domains == None, "You can't specify both cell_domains or facet_domains"
@@ -56,6 +59,88 @@ def _init_label(measure):
 
     return label
 
+def duplicate_meshfunction(mf, new_mesh):
+    "Duplicate meshfunction defined on a different mesh"
+    # Rebuild meshfunction
+    mesh = mf.mesh()
+    dim = mf.dim()
+    
+    if isinstance(mf, MeshFunctionSizet):
+        f = MeshFunction("size_t", new_mesh, dim)
+        dtype = np.uintp
+    elif isinstance(mf, MeshFunctionDouble):
+        f = MeshFunction("double", new_mesh, dim)
+        dtype = np.float_
+    elif isinstance(mf, MeshFunctionBool):
+        f = MeshFunction("bool", new_mesh, dim)
+        dtype = np.bool
+    elif isinstance(mf, MeshFunctionInt):
+        f = MeshFunction("int", new_mesh, dim)
+        dtype = np.int
+
+    # Midpoint of old mesh
+    connectivity = mesh.topology()(dim,0)().reshape(mesh.num_entities(dim), -1)
+    midpoints = np.mean(mesh.coordinates()[connectivity], axis=1)
+    indices = np.lexsort(midpoints.T[::-1])
+    
+    sorted_midpoints = midpoints[indices]
+    values = mf.array()[indices]
+    
+    gdim = midpoints.shape[1]
+    sorted_midpoints = sorted_midpoints.flatten()
+    sorted_midpoints = gather(sorted_midpoints, 0)
+    sorted_midpoints = np.hstack(sorted_midpoints).flatten()
+
+    sorted_midpoints = broadcast(sorted_midpoints, 0)
+    _sorted_midpoints = []
+
+    i = 0
+    for arr in sorted_midpoints:
+        _sorted_midpoints.append(arr)
+    sorted_midpoints = np.reshape(sorted_midpoints, newshape=(-1, gdim))
+    values = gather(values, 0)
+    values = np.hstack(values).flatten()
+
+    values = broadcast(values, 0)
+    
+    # Workaround for bug in np.uint64
+    if dtype==np.uintp:
+        M = max(values)
+        if M == dtype(-1):
+
+            values[values == M] = -1
+    
+    values = values.astype(dtype)
+    _values = []
+    for arr in values:
+        _values.append(arr)
+    values = np.array(_values, dtype=dtype)
+    
+    indices = np.lexsort(sorted_midpoints.T[::-1])
+    values = values[indices]
+    sorted_midpoints = sorted_midpoints[indices]
+
+    # Now has full list of midpoint+values on all processes
+    
+    # Sort midpoints on new mesh on current process
+    connectivity = new_mesh.topology()(dim,0)().reshape(new_mesh.num_entities(dim), -1)
+    midpoints = np.mean(new_mesh.coordinates()[connectivity], axis=1)
+    indices = np.lexsort(midpoints.T[::-1])
+    midpoints = midpoints[indices]
+
+    idx = 0
+    omp = sorted_midpoints[idx]
+    tol = 1e-8
+
+    for i, mp in enumerate(midpoints):
+        for j in xrange(gdim):
+            while omp[j] < mp[j]-tol:
+                idx += 1
+                omp[:] = sorted_midpoints[idx]
+        f[int(indices[i])] = values[idx]
+
+    return f
+
 
 class DomainAvg(MetaField):
     """Compute the domain average for a specified domain. Default to computing
@@ -84,9 +169,14 @@ class DomainAvg(MetaField):
 
         # Find mesh/domain
         if isinstance(self.dI.subdomain_data(), MeshFunctionSizet):
-            mesh = self.dI.subdomain_data().mesh()
-        elif isinstance(u, Function):
-            mesh = u.function_space().mesh()
+            mf = self.dI.subdomain_data()
+            if mf.mesh().id() != u.domain().data().id():
+                mf = duplicate_meshfunction(mf, u.domain().data())
+            mesh = mf.mesh()
+
+            self.dI = self.dI.reconstruct(domain=mesh, subdomain_data=mf)
+        else:
+            mesh = u.domain().data()
 
         if not self.dI.domain():
             self.dI = self.dI.reconstruct(domain=mesh)

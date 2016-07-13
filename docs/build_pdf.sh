ab0200d3c5be004af9733c7362f31f4885d771a2
Øyvind Evju <oyvinev@simula.no>
2016-07-13 14:43:35 +0200

    Added script to generate pdf output

---
+++ docs/build_pdf.sh
@@ -0,0 +1,17 @@
+#/usr/bin/env bash
+
+rm -rf _build
+sphinx-build -b latex . _build
+if [ $? -eq 0 ];
+    then
+    if [ $1 == "show" ]
+        then
+            cd _build
+            make
+            cd ..
+            if [ $? -eq 0 ];
+                then
+                    evince _build/cbcpost.pdf &
+            fi
+    fi
+fi
